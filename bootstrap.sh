6ff68c9b735480bfa1d5a82d271bec798b901ec3
rolindroy <rolind.roy@gmail.com>
2016-08-31 10:30:23 +0530

    remove maven ansible-galaxy role.

--- bootstrap.sh
+++ bootstrap.sh
@@ -82,13 +82,12 @@ ansible all -m ping --private-key=$BT_KeyPath/$BT_Ssh_KeyName || bootstrap_handl
 bootstrap_handler $BT_OK "Ansible successfully installed and configured."
 
 bootstrap_logger "Dowloading files to install and configure java"
-ansible-galaxy install geerlingguy.java -p ./roles/
+ansible-galaxy install geerlingguy.java -f -p ./roles/
+mv ./roles/geerlingguy.java ./roles/java
 
 bootstrap_logger "Dowloading files to install and configure Mysql Database"
 ansible-galaxy install geerlingguy.mysql -p ./roles/
-
-bootstrap_logger "Dowloading files to install and configure Maven"
-ansible-galaxy install tecris.maven -p ./roles/
+mv ./roles/geerlingguy.mysql ./roles/mysql
 
 bootstrap_logger "Running ansible-playbook bootstrap-setup.yml"
 ansible-playbook -i hosts bootstrap-setup.yml || bootstrap_handler $BT_Error "Execute ansible-playbook -vvvv -i hosts bootstrap-setup.yml" $BT_Die
